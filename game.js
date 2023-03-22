88a17a086226ff05a74bb98a02b2752868693a42
Samie Bee <sbeefm@gmail.com>
2023-03-22 18:29:56 -0400

    Import Cannon Demo Scene

--- game.js
+++ game.js
@@ -1,23 +1,17 @@
 
 
-import {player, objects, textures} from './3gw/main.js'
-
 // create solid, textured ground surface
-var ground = objects.prism(
+var ground = prism(
   -10, -10, -10, 
   10, -8, 10, 
-  textures.checkerboard([34, 34, 34, 255], [0, 0, 0, 255], 128) 
+  checkerboard([120, 120, 120, 255], [0, 0, 0, 255], 128) 
 )
 
 // create solid floating cubes
-objects.prism(5, 5, 5, 6, 6, 6, textures.stripes([200, 0, 200, 255], [40, 0, 200, 255], 256))
-objects.prism(3, 5, 5, 4, 6, 6, textures.solidcolor([0, 255, 0, 100]))
+prism(5, 5, 5, 6, 6, 6, stripes([200, 0, 200, 255], [40, 0, 200, 255], 256))
+prism(3, 5, 5, 4, 6, 6, solidcolor([0, 255, 0, 100]))
 
-var retexGround = (entity)=>{
-  ground.texture = textures.checkerboard([255, 0, 0, 255], [0, 0, 255, 255], 5)
-}
-
-objects.interactive(3,3,3, textures.solidcolor([200, 40, 40, 255]))
+interactive(3,3,3, solidcolor([200, 40, 40, 255]))
 
 // set player spawn point
 // position specifies where the player is standing
@@ -25,5 +19,8 @@ objects.interactive(3,3,3, textures.solidcolor([200, 40, 40, 255]))
 // optionally specify look direction, defaults to 0, 0, 1
 player(0, 0, 0, 0, 0, 1)
 
+// var retexGround = (entity)=>{
+//   ground.texture = checkerboard([255, 0, 0, 255], [0, 0, 255, 255], 5)
+// }
 // create transient invisible trigger zone
 //objects.trigger( -10, 6, -10, -3, 0, 10, false, retexGround)
