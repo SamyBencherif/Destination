ea152d33b7b80c4ee39949a13808d11dd1388fd0
Samie Bee <sbeefm@gmail.com>
2023-09-24 11:44:30 -0400

    adjust demo scene

--- world.c
+++ world.c
@@ -6,7 +6,8 @@ void world()
   for (int x=-10; x<11; x++)
     for (int z=-10; z<11; z++)
     {
-      float displacement = cos(x/10.+z/10.+6*t);
+      float displacement = (1+(abs(x))*(abs(z))/10);
+      displacement *= (.5+.5*cos(x/10.+z/10.+6*t));
       Vector3 position = {10*x, displacement/2, 10*z};
       Color color = {200, 40+5*x, 40*z, 255};
       DrawCube(position, 3, 1+displacement, 3, color); 
